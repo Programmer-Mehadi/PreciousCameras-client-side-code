89ac2f4a2fe7096fc3517f25b89c2b7b85e1cc6a
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-11-28 18:09:20 +0600

    time format changed.

--- src/Pages/Dashboard/AddProduct/AddProduct.js
+++ src/Pages/Dashboard/AddProduct/AddProduct.js
@@ -23,20 +23,45 @@ const AddProduct = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
     const [data, setData] = useState("");
     const imageHostKey = process.env.REACT_APP_imgbb_key;
-    function make2Digits(num) {
-        return num.toString().padStart(2, '0');
-    }
 
-    function formatDate(date) {
-        return [
-            date.getFullYear(),
-            make2Digits(date.getMonth() + 1),
-            make2Digits(date.getDate()),
-        ].join('-');
+    // function make2Digits(num) {
+    //     return num.toString().padStart(2, '0');
+    // }
+
+    // function formatDate(date) {
+    //     return [
+    //         date.getFullYear(),
+    //         make2Digits(date.getMonth() + 1),
+    //         make2Digits(date.getDate()),
+    //     ].join('-');
+    // }
+
+    const formatTime = () => {
+        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var d = new Date();
+        var day = days[d.getDay()];
+        var hr = d.getHours();
+        var min = d.getMinutes();
+        if (min < 10) {
+            min = "0" + min;
+        }
+        var ampm = "am";
+        if (hr > 12) {
+            hr -= 12;
+            ampm = "pm";
+        }
+        var date = d.getDate();
+        var month = months[d.getMonth()];
+        var year = d.getFullYear();
+
+        const x = day + " " + hr + ":" + min + ampm + " " + date + " " + month + " " + year;
+        console.log(x);
+        return x;
     }
 
     const handleAdd = (data) => {
-        const date = formatDate(new Date());
+        const date = formatTime();
         data['userName'] = user?.displayName;
         data['date'] = date;
         data['email'] = user?.email;
